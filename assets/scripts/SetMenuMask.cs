e26ff1da6b0bd1fe8f2da6dfaf8d6388f44a476d
seb776 <sebastien.maire@epitech.eu>
2023-04-29 20:40:20 +0200

    Added funn shader for menu

--- assets/scripts/SetMenuMask.cs
+++ assets/scripts/SetMenuMask.cs
@@ -1,11 +1,15 @@
 using Godot;
 using System;
 
-public partial class new_script : Node
+public partial class SetMenuMask : TextureRect
 {
+	[Export]
+	public Texture MaskTexture;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		var shaderMat = this.Material as ShaderMaterial;
+		shaderMat.SetShaderParameter("maskTexture", MaskTexture);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
