bc7999dfd3d234b6f431a00e72e4863e35c53e27
seb776 <sebastien.maire@epitech.eu>
2023-04-29 23:48:36 +0200

    Added sounds and finished menu screen

---
+++ assets/scripts/HandleMenu.cs
@@ -0,0 +1,86 @@
+using Godot;
+using System;
+
+public partial class HandleMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        CreditsTexture.Visible = false;
+    }
+
+	private bool _isOnDeliver = true;
+	private bool _isShowingCredits = false;
+
+	[Export]
+	private Label LabelDeliver;
+    [Export]
+    private Label LabelCredits;
+
+	[Export]
+	private TextureRect CreditsTexture;
+
+    [Export]
+    private ColorRect Fader;
+
+    private async void FadeOut()
+    {
+        float duration = 2.0f;
+        float stepspersec = 30.0f;
+
+        float steptime = 1.0f/ stepspersec;
+
+        float time = 0.0f;
+        Fader.Color = new Color(0, 0, 0, 0);
+        while (time < duration)
+        {
+            await ToSignal(GetTree().CreateTimer(steptime), "timeout");
+            Fader.Color = new Color(0, 0, 0, time / duration);
+            time += steptime;
+        }
+        Fader.Color = new Color(0, 0, 0,1);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{
+		if (_isShowingCredits)
+		{
+            if (Input.IsActionJustReleased("echap"))
+            {
+                _isShowingCredits = false;
+                CreditsTexture.Visible = false;
+            }
+		}
+		else
+		{
+            if (Input.IsActionJustReleased("forward") || Input.IsActionJustReleased("backward"))
+                _isOnDeliver = !_isOnDeliver;
+
+            if (_isOnDeliver)
+            {
+                LabelDeliver.Text = ">";
+                LabelCredits.Text = "";
+            }
+            else
+            {
+                LabelDeliver.Text = "";
+                LabelCredits.Text = ">";
+            }
+
+            if (Input.IsActionJustReleased("ente" +
+                "" +
+                "r"))
+            {
+                if (_isOnDeliver)
+                    FadeOut();// TODO start game
+                else
+                {
+                    CreditsTexture.Visible = true;
+                    _isShowingCredits = true;
+                }
+            }
+
+        }
+    }
+}
