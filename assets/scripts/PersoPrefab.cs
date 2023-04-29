2a65963ff5b886747758b41bbe07b5a120083d3d
seb776 <sebastien.maire@epitech.eu>
2023-04-29 18:38:20 +0200

    Added player assets and code

---
+++ assets/scripts/PersoPrefab.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class PersoPrefab : Node2D
+{
+	private float moveSpeed = 200f;
+	[Export]
+	private CharacterBody2D CharacterBody;
+	[Export]
+	private AnimatedSprite2D AnimatedSprite;
+
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+		Vector2 move = Vector2.Zero;
+		if (Input.IsActionPressed("right"))
+		{
+			AnimatedSprite.Scale = new Vector2(1, 1);
+			move.X = this.moveSpeed;
+		}
+		if (Input.IsActionPressed("left"))
+		{
+			AnimatedSprite.Scale = new Vector2(-1, 1);
+			move.X = -this.moveSpeed;
+		}
+		if (move.Length() > 0.1)
+			AnimatedSprite.Play("Walk");
+		else
+			AnimatedSprite.Play("Idle");
+
+		CharacterBody.Velocity = move;
+		CharacterBody.MoveAndSlide();
+	}
+}
