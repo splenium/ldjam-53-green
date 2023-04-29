f5267c63b762505730db536790c5354050bf07e6
Herbigniaux <b.herbigniaux@hotmail.fr>
2023-04-29 19:53:33 +0200

    First scene of spaceships

---
+++ assets/scripts/Asteroid.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+
+public partial class Asteroid : Area2D
+{
+    [Export]
+    private AsteroidSize _size = AsteroidSize.Large;
+
+    private float speed;
+    private enum AsteroidSize { Large, Medium, Small }
+    private Vector2 movementVector = new(0, -1);
+
+    private Sprite2D _sprite;
+    private CollisionShape2D _collisionShape;
+    public override void _Ready()
+    {
+        _sprite = GetNode<Sprite2D>("Sprite2D");
+        _collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+
+        Rotation = (float)GD.RandRange(0, 2d * MathF.PI);
+        switch (_size)
+        {
+            case AsteroidSize.Large:
+                speed = GD.RandRange(50, 100);
+                _sprite.Texture = GD.Load("res://assets/sprites/space/meteors/meteorBrown_big1.png") as Texture2D;
+                _collisionShape.Shape = GD.Load("res://ressources/asteroid_cshape_big.tres") as Shape2D;
+                break;
+            case AsteroidSize.Medium:
+                speed = GD.RandRange(100, 150);
+                _sprite.Texture = GD.Load("res://assets/sprites/space/meteors/meteorBrown_med1.png") as Texture2D;
+                _collisionShape.Shape = GD.Load("res://ressources/asteroid_cshape_medium.tres") as Shape2D;
+                break;
+            case AsteroidSize.Small:
+                speed = GD.RandRange(150, 200);
+                _sprite.Texture = GD.Load("res://assets/sprites/space/meteors/meteorBrown_small1.png") as Texture2D;
+                _collisionShape.Shape = GD.Load("res://ressources/asteroid_cshape_small.tres") as Shape2D;
+                break;                   
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GlobalPosition += movementVector.Rotated(Rotation) * speed * (float)delta;
+
+        var screenSize = GetViewportRect().Size;
+        if (Position.Y < 0 || Position.Y > screenSize.Y 
+            && Position.X < 0 || Position.X > screenSize.X )
+        {
+            GD.Print("Sortie de l'ecrans... destruction de l'objet!");
+            QueueFree();
+        }
+    }
+}
---
+++ assets/scripts/Spaceship.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+public partial class Spaceship : CharacterBody2D
+{
+    private float _acceleration = 10f;
+    private float _maxSpeed = 350f;
+    private double _rotationSpeed = 100f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 input_vector = new(0, Input.GetAxis("forward", "backward"));
+        
+        Velocity += input_vector.Rotated(Rotation) * _acceleration;
+        Velocity = Velocity.LimitLength(_maxSpeed);
+        if (Input.IsActionPressed("right"))
+        {
+            Rotate((float)Mathf.DegToRad(_rotationSpeed * delta));
+        }
+        if (Input.IsActionPressed("left"))
+        {
+            Rotate((float)Mathf.DegToRad(-_rotationSpeed * delta));
+        }
+        if (input_vector.Y == 0)
+        {
+            Velocity = Velocity.MoveToward(Vector2.Zero, 3);
+        }
+        MoveAndSlide();
+    }
+}
