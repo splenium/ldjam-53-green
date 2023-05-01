0b90a4c76c210a7a6c0825bb82853803f5535791
Herbigniaux <b.herbigniaux@hotmail.fr>
2023-05-01 16:28:48 +0200

    Add new asteroids

--- assets/scripts/AsteroidsManager.cs
+++ assets/scripts/AsteroidsManager.cs
@@ -4,10 +4,11 @@ using System.Linq;
 
 public partial class AsteroidsManager : Node
 {
-    private PackedScene _asteroidPrefabs;
     private Vector2[] _offSets;
 
-    private PackedScene[] _asteroidsPrefabs;
+    [Export]
+    private PackedScene[] _asteroidPrefabs;
+
     [Export]
     private Timer spawnTimer;
 
@@ -17,16 +18,15 @@ public partial class AsteroidsManager : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-        _asteroidPrefabs = GD.Load("res://assets/prefabs/asteroid.tscn") as PackedScene;
         _offSets = new Vector2[] { new Vector2(1, 0), new Vector2(0, 1), new Vector2(-1, 0), new Vector2(0, -1) };
     }
 
     void _on_asteroids_timer_timeout()
     {
-        var instance = _asteroidPrefabs.Instantiate() as Node2D;
+        var instance = _asteroidPrefabs[(int)GD.RandRange(0, _asteroidPrefabs.Length - 1)].Instantiate() as Node2D;
         AddChild(instance);
 
-        Vector2 spawnPosition = playerSpaceships.GlobalPosition + _offSets[(int)GD.RandRange(0, _offSets.Count()-1)] * 645;
+        Vector2 spawnPosition = playerSpaceships.GlobalPosition + _offSets[(int)GD.RandRange(0, _offSets.Length - 1)] * 645;
         instance.GlobalPosition = spawnPosition;
 
         spawnTimer.Start(spawnTimer.WaitTime);
