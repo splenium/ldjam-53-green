c4fc691a558db1073909c1f1629c63a27d4cce1e
seb776 <sebastien.maire@epitech.eu>
2023-05-01 15:40:01 +0200

    Indent

--- assets/scripts/HandleMenu.cs
+++ assets/scripts/HandleMenu.cs
@@ -1,100 +1,100 @@
-﻿using Godot;
+using Godot;
 using System;
 
 public partial class HandleMenu : Control
 {
-    private GameManager _gameManager { get; set; }
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	private GameManager _gameManager { get; set; }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-        _gameManager = GetNode<GameManager>("/root/GameManager");
-        DisplayCredit(_gameManager.MustShowCreditsFirst);
-    }
+		_gameManager = GetNode<GameManager>("/root/GameManager");
+		DisplayCredit(_gameManager.MustShowCreditsFirst);
+	}
 
 	private bool _isOnDeliver = true;
 	private bool _isShowingCredits = false;
 
 	[Export]
 	private Label LabelDeliver;
-    [Export]
-    private Label LabelCredits;
+	[Export]
+	private Label LabelCredits;
 
 	[Export]
 	private TextureRect CreditsTexture;
 
-    [Export]
-    private ColorRect Fader;
+	[Export]
+	private ColorRect Fader;
 
-    private async void FadeOut()
-    {
-        float duration = 2.0f;
-        float stepspersec = 30.0f;
+	private async void FadeOut()
+	{
+		float duration = 2.0f;
+		float stepspersec = 30.0f;
 
-        float steptime = 1.0f/ stepspersec;
+		float steptime = 1.0f/ stepspersec;
 
-        float time = 0.0f;
-        Fader.Color = new Color(0, 0, 0, 0);
-        while (time < duration)
-        {
-            await ToSignal(GetTree().CreateTimer(steptime), "timeout");
-            Fader.Color = new Color(0, 0, 0, time / duration);
-            time += steptime;
-        }
-        Fader.Color = new Color(0, 0, 0,1);
-        await ToSignal(GetTree().CreateTimer(1), "timeout");
-        StartGame();
-    }
+		float time = 0.0f;
+		Fader.Color = new Color(0, 0, 0, 0);
+		while (time < duration)
+		{
+			await ToSignal(GetTree().CreateTimer(steptime), "timeout");
+			Fader.Color = new Color(0, 0, 0, time / duration);
+			time += steptime;
+		}
+		Fader.Color = new Color(0, 0, 0,1);
+		await ToSignal(GetTree().CreateTimer(1), "timeout");
+		StartGame();
+	}
 
-    private void StartGame()
-    {
-        _gameManager.NewGame();
-        _gameManager.LandingOn(_gameManager.PlanetPosition);
-    }
+	private void StartGame()
+	{
+		_gameManager.NewGame();
+		_gameManager.LandingOn(_gameManager.PlanetPosition);
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
 	{
 		if (_isShowingCredits)
 		{
-            if (Input.IsActionJustReleased("echap"))
-            {
-                DisplayCredit(false);
-            }
+			if (Input.IsActionJustReleased("echap"))
+			{
+				DisplayCredit(false);
+			}
 		}
 		else
 		{
-            if (Input.IsActionJustReleased("forward") || Input.IsActionJustReleased("backward"))
-                _isOnDeliver = !_isOnDeliver;
+			if (Input.IsActionJustReleased("forward") || Input.IsActionJustReleased("backward"))
+				_isOnDeliver = !_isOnDeliver;
 
-            if (_isOnDeliver)
-            {
-                LabelDeliver.Text = "X";
-                LabelCredits.Text = "";
-            }
-            else
-            {
-                LabelDeliver.Text = "";
-                LabelCredits.Text = "X";
-            }
+			if (_isOnDeliver)
+			{
+				LabelDeliver.Text = "X";
+				LabelCredits.Text = "";
+			}
+			else
+			{
+				LabelDeliver.Text = "";
+				LabelCredits.Text = "X";
+			}
 
-            if (Input.IsActionJustReleased("enter"))
-            {
-                if (_isOnDeliver)
-                {
-                   FadeOut();
-                }
-                else
-                {
-                    DisplayCredit(true);
-                }
-            }
+			if (Input.IsActionJustReleased("enter"))
+			{
+				if (_isOnDeliver)
+				{
+				   FadeOut();
+				}
+				else
+				{
+					DisplayCredit(true);
+				}
+			}
 
-        }
-    }
+		}
+	}
 
-    private void DisplayCredit(bool display)
-    {
-        CreditsTexture.Visible = display;
-        _isShowingCredits = display;
-    }
+	private void DisplayCredit(bool display)
+	{
+		CreditsTexture.Visible = display;
+		_isShowingCredits = display;
+	}
 }
