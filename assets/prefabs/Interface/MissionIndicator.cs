2976edd82a7c4881799558796de757564489290a
BuildTools <unconfigured@null.spigotmc.org>
2023-05-01 17:59:52 +0200

    small changes

--- assets/prefabs/Interface/MissionIndicator.cs
+++ assets/prefabs/Interface/MissionIndicator.cs
@@ -3,13 +3,26 @@ using System;
 
 public partial class MissionIndicator : HBoxContainer
 {
+	[Export]
+	private Label planetLabel;
+
+	private GameManager _gameManager;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+        _gameManager = GetNode<GameManager>("/root/GameManager");
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta)
+    {
+        if (_gameManager.MissionInProgress())
+        {
+            this.Visible = true;
+            planetLabel.Text = _gameManager.PlanetNames[_gameManager.MissionPlanetTarget()];
+        }
+        else
+        {
+            this.Visible = false;
+        }
+    }
 }
