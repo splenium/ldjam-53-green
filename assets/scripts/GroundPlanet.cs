833982b994fdffdb428efd44407490d61624bff1
BuildTools <unconfigured@null.spigotmc.org>
2023-05-01 23:30:50 +0200

    wefjjfw\

--- assets/scripts/GroundPlanet.cs
+++ assets/scripts/GroundPlanet.cs
@@ -5,9 +5,4 @@ public partial class GroundPlanet : Node2D
 {
 	[Export]
 	public int PlanetId;
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 }
